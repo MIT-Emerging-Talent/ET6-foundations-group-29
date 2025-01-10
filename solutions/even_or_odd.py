4c31378b0bb9c4fc10b97721f750fdbc3a875d09
Mohammad Ahmadi <mohammad.srak1999@gmail.comgit config --global user.email mohammad.srak1999@gmail.com>
2025-01-10 21:35:41 +0430

    even_or_odd

--- solutions/even_or_odd.py
+++ solutions/even_or_odd.py
@@ -1,4 +1,6 @@
-# Finction to check if a number is odd or even
+"""Finction to check if a number is odd or even
+@author: msrak
+"""
 
 def check_even_or_odd(number):
     if number % 2 == 0:
@@ -6,7 +8,7 @@ def check_even_or_odd(number):
     else:
         return f"number is odd"
 
-# for the user
-6num = int(input("Enter a number"))
+
+num = int(input("Enter a number"))
 result = check_even_or_odd(num)
 print(result)
