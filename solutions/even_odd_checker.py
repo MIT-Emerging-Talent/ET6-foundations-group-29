0e426a0288817dda73e7baca27db9c0f0676b204
Mohammad Ahmadi <mohammad.srak1999@gmail.comgit config --global user.email mohammad.srak1999@gmail.com>
2025-01-12 09:57:13 +0430

    test_even_odd

---
+++ solutions/even_odd_checker.py
@@ -0,0 +1,26 @@
+"""
+Author: @msrak
+Created on: January 8, 2024.
+"""
+
+def check_even_or_odd(number):
+    """
+    The check_even_or_odd function takes a number as input and determines whether it is even or odd.
+    It returns a string indicating the result.
+
+    PARAMETERS:
+    number (int): The number to be checked for even or odd.
+
+    RETURN VALUES:
+    str: "even" if the number is divisible by 2, "odd" if it is not.
+    """
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+
+# Example usage
+num = int(input("Enter a number: "))
+result = check_even_or_odd(num)
+print(f"The number {num} is {result}.")
---
+++ solutions/test_check_even_or_odd.py
@@ -0,0 +1,32 @@
+# test_check_even_or_odd.py
+import unittest
+from even_odd_checker import check_even_or_odd
+
+class TestCheckEvenOrOdd(unittest.TestCase):
+    
+    def test_even_number(self):
+        result = check_even_or_odd(4)
+        self.assertEqual(result, "even")
+
+    def test_odd_number(self):
+        result = check_even_or_odd(3)
+        self.assertEqual(result, "odd")
+
+    def test_zero(self):
+        result = check_even_or_odd(0)
+        self.assertEqual(result, "even")
+
+    def test_negative_even_number(self):
+        result = check_even_or_odd(-2)
+        self.assertEqual(result, "even")
+
+    def test_negative_odd_number(self):
+        result = check_even_or_odd(-3)
+        self.assertEqual(result, "odd")
+
+    def test_invalid_input(self):
+        with self.assertRaises(TypeError):
+            check_even_or_odd("not_a_number")
+
+if __name__ == "__main__":
+    unittest.main()
